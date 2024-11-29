40ed472b9290c728ba723ae4acfb41dc9c2cb054
Diego <gomeztorres.diegoandres@gmail.com>
2024-11-29 16:50:23 +0100

    deploiment

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Portefolio Diego</title>
       </Head>
       <Component {...pageProps} />
     </>
