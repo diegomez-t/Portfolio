125d92cc6893f1724197d5d48fd2e1ac69fec965
Diego <gomeztorres.diegoandres@gmail.com>
2024-12-02 14:06:34 +0100

    Menu

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,19 @@
 import styles from '../styles/Home.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faUser, faFolderOpen, faNewspaper } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
   return (
     <div className={styles.main}>
-        <nav>
-          <FontAwesomeIcon icon={'house'} /> 
+        <nav className={styles.menu}>
+          <FontAwesomeIcon icon={faNewspaper} className={styles.icon}/> 
+          <FontAwesomeIcon icon={faUser} className={styles.icon}/> 
+          <FontAwesomeIcon icon={faFolderOpen} className={styles.icon}/> 
+          <FontAwesomeIcon icon={faGithub} className={styles.icon}/> 
         </nav>
         <div className={styles.center}>
-          <h1 className={styles.title}>Mon Portfolio</h1>
+          <h1 className={styles.title}>Créer des expériences digitales uniques, où la performance rencontre le design.</h1>
         </div>
     </div>
   );
