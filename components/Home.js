ad75ce4fc001ddc96eea9e290b7ce8fb59488565
Diego <gomeztorres.diegoandres@gmail.com>
2024-12-02 13:19:48 +0100

    fluter

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,12 @@
 import styles from '../styles/Home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Home() {
   return (
     <div className={styles.main}>
+        <nav>
+          <FontAwesomeIcon icon={'house'} /> 
+        </nav>
         <div className={styles.center}>
           <h1 className={styles.title}>Mon Portfolio</h1>
         </div>
