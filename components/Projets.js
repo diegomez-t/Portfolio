eb269b85823b3144a99f744c1aa6e6edc29db5dd
Diego <gomeztorres.diegoandres@gmail.com>
2024-12-20 10:16:11 +0100

    correction de styles

--- components/Projets.js
+++ components/Projets.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Projets.module.css'
 import React, { useEffect, useState } from 'react';
-import Immage from 'next/image'
+import Image from 'next/image';
 
 function Projets() {
 
@@ -11,22 +11,12 @@ function Projets() {
     {title:'To do list', img:'/img-todolist.png',},
   ]
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % projetsData.length);
-    }, 3000);
-
-    return () => clearInterval(interval); // Clean up interval on component unmount
-  }, []);
-  
-
   const projets = projetsData.map((data,i)=>{
     return (
         <div className={styles.card} key={i}>
-          <Immage  src={data.img} alt={data.title} width={300} height={200}/>
-          <h4 className={styles.text}>{data.title}</h4>
+            <Image src={data.img} alt={data.title} width={310} height={234} className={styles.img}/>
+            <p className={styles.text}>{data.title}</p>
+          <button className={styles.button}>Aller vers la page</button>
         </div>
     )
   })
