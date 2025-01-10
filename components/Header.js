58aab4bef4de108443c269fbe2cb7a4dd09374e5
Diego <gomeztorres.diegoandres@gmail.com>
2025-01-10 19:19:14 +0100

    modification header

--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,43 @@
 import styles from '../styles/Header.module.css';
 import { Home, Person, Folder, GitHub } from '@mui/icons-material';
 import { useRouter } from 'next/router';
+import React, { useState } from "react";
 
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuData = [
+    {href: "/", lebel:'Home'},
+    {href: '/projets', lebel:'Projets'},
+    {href: "/profil", lebel:'Information'},
+    {href: "/social", lebel:'Services'},
+  ]
   const router = useRouter();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const handleNavigation = (path) => {
     router.push(path);
   };
+  const menu = menuData.map((data, i) => {
+    return (
+      <button
+        key={i}
+        onClick={() => handleNavigation(data.href)}
+        data-title={data.lebel}
+        className={styles.icon}
+      >
+        {data.lebel}
+      </button>
+    );
+  })  
   return (
     <div className={styles.header}>
       <h1 className={styles.title}>Diego</h1>
       <nav className={styles.menu}>
-          <button
-          onClick={() => handleNavigation('/')}
-          data-title="Home"
-          className={styles.icon}
-          >
-          Home
-          </button>
-          <button
-          onClick={() => handleNavigation('/projets')}
-          data-title="Projets"
-          className={styles.icon}
-          >
-          Projets
-          </button>
-          <button
-          onClick={() => handleNavigation('/profil')}
-          data-title="CV"
-          className={styles.icon}
-          >
-          Information
-          </button>
-          <button
-          onClick={() => handleNavigation('/social')}
-          data-title="Github"
-          className={styles.icon}
-          >
-          Services
-          </button>
+        {menu}
       </nav>
           <button className={styles.contact}>
           Contact
