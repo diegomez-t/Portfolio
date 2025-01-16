c4191994a3820ff6088b2ef48def05a2e52069d7
Diego <gomeztorres.diegoandres@gmail.com>
2025-01-16 09:00:29 +0100

    modification style modal projets

--- components/Projets.js
+++ components/Projets.js
@@ -12,7 +12,7 @@ function Projets() {
     {title: 'Morning News', img: '/img-morningnews.png', link: 'https://morning-news-alpha.vercel.app', desc: 'MorningNews permet de consulter les dernières actualités Tech Crunch, sauvegarder vos articles préférés, et les retrouver facilement grâce à un système de compte utilisateur et une interface intuitive.'},
     {title:'Star wars', img:'/img-starwars.png', link: 'https://starwars-theme.vercel.app', desc: 'Star Wars App offre une expérience immersive dans la saga grâce à une gestion dynamique du thème en React, incluant un mode sombre pour les fans de l’univers galactique.'},
     {title:'Time Tracker', img:'/img-TimeTracker.png', desc: 'Le projet TimeTracker est une application qui permet de gérer plusieurs activités indépendantes, chacune avec son propre chronomètre, afin de suivre efficacement le temps passé sur différentes tâches.'},
-    {title:'To do list', img:'/img-todolist.png', desc: 'L\'application To Do List permet de gérer efficacement les tâches quotidiennes grâce à React et Redux. Elle offre des fonctionnalités avancées pour ajouter, organiser, et suivre vos activités avec flexibilité.'},
+    {title:'To do list', img:'/img-todolist.png',  desc: 'L\'application To Do List permet de gérer efficacement les tâches quotidiennes grâce à React et Redux. Elle offre des fonctionnalités avancées pour ajouter, organiser, et suivre vos activités avec flexibilité.'},
     {title:'Memory game', img:'/memorygame.png', link: 'https://memorygame-two-sigma.vercel.app', desc: 'Le projet MemoryGame est un jeu de mémoire interactif qui utilise les concepts fondamentaux de React, tels que le cycle de vie des composants, la gestion des états, et l’inverse data flow.'},
   ]
 
@@ -50,10 +50,14 @@ function Projets() {
                 <button className={styles.button} onClick={handleOk}>Aller sur la page</button>
               </a>,
             ]}
+            className={styles.modal}
           >
-            <p className={styles.text}>{currentProject?.title}</p>
-            <p className={styles.textI}>{currentProject?.desc}</p>
-            <Image src={currentProject?.img} alt={currentProject?.title} width={310} height={200} className={styles.img}/>
+            <h1 className={styles.textModal}>{currentProject?.title}</h1>
+            <p className={styles.textInnerModal}>{currentProject?.desc}</p>
+            <h3 className={styles.textModal}>Technologies utilisées</h3>
+            <div className={styles.images}>
+              <Image src={currentProject?.img} alt={currentProject?.title} width={310} height={200} className={styles.imgModal}/>
+            </div>
           </Modal>
         </div>
     )
